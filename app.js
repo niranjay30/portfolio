03070dca42385e4ed5228c378f113e11e48cb612
niranjay <niranjay.bhosale@gmail.com>
2020-09-05 12:29:52 +0530

    initial commit
    start of deployment

---
+++ app.js
@@ -0,0 +1,50 @@
+//jshint esversion:6
+
+const express = require('express');
+// const bodyParser = require('body-parser');
+// const ejs = require('ejs');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const allProjects = ["tindog", "dice", "simon", "drumkit"];
+
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+
+app.get("/projects", function(req, res){
+  res.render("projects");
+});
+
+
+app.get("/contact", function(req, res){
+  res.render("contact");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000...");
+});
